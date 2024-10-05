3b52d0cc010fd00430133b2eb14de5ecf5fd890e
Малиновский Максим <ritmax074@gmail.com>
2024-10-05 07:40:28 +0300

    Обновление

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-  print('Привет мир!');
+  print('Который час?');
+  var n:=readinteger;
+  if n in 4..10 then
+  print('Доброе утро, мир!') else
+    if n in 11..16 then
+  print('Добрый день, мир!') else
+    if n in 17..22 then
+  print('Добрый вечер, мир!') else
+    print('Доброй ночи, мир!');
 end.
